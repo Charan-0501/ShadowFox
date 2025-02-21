1cace459c5bb020a3264944cd0eab7736a41ca6a
shiva charan <shivacharanreddykandati@gmail.com>
2025-02-21 23:32:39 +0530

    Calculator is ready

--- Console_Calculator/src/main/java/org/example/Main.java
+++ Console_Calculator/src/main/java/org/example/Main.java
@@ -1,8 +1,44 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("Hello world!");
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean continues = true;
+        do {
+            System.out.println("Welcome to Calci, What do you want to perform ?");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println(" 1. +\n 2. -\n 3. *\n 4. /\n 5. %");
+
+            int n = sc.nextInt();
+            System.out.println("Enter two numbers :");
+            double a = sc.nextInt();
+            double b = sc.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.println("Addition of " + a + " and " + b + " is : " + (a + b));
+                    break;
+                case 2:
+                    System.out.println("Subtraction of " + a + " and " + b + " is : " + (a - b));
+                    break;
+                case 3:
+                    System.out.println("Multiplication of " + a + " and " + b + " is : " + (a * b));
+                    break;
+                case 4:
+                    System.out.println("Division of " + a + " and " + b + " is : " + (a/b));
+                    break;
+                case 5:
+                    System.out.println("Modulo of " + a + " and " + b + " is : " + (a % b));
+                    break;
+                default:
+                    System.out.println("Invalid Operation");
+            }
+            System.out.println( "Please enter yes for another calculation ,otherwise no ");
+            String isContinues = sc.nextLine();
+            if(isContinues.equalsIgnoreCase("no")) continues=false;
+
+
+        }while(continues);
     }
 }
