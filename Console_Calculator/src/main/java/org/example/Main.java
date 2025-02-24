5e6c3d485ac8249f9c31b8bd975a5373d5515836
shiva charan <shivacharanreddykandati@gmail.com>
2025-02-24 20:25:05 +0530

    Handled edge cases

--- Console_Calculator/src/main/java/org/example/Main.java
+++ Console_Calculator/src/main/java/org/example/Main.java
@@ -9,12 +9,14 @@ public class Main {
         do {
             System.out.println("Welcome to Calci, What do you want to perform ?");
             System.out.println("-----------------------------------------------------------");
-            System.out.println(" 1. +\n 2. -\n 3. *\n 4. /\n 5. %");
+            System.out.println(" 1. Addition +\n 2. Subtraction-\n 3. Multiplication *\n 4. Division /\n 5. Modulo % ");
 
             int n = sc.nextInt();
             System.out.println("Enter two numbers :");
-            double a = sc.nextInt();
-            double b = sc.nextInt();
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            //sc.nextLine();
+
             switch (n) {
                 case 1:
                     System.out.println("Addition of " + a + " and " + b + " is : " + (a + b));
@@ -26,10 +28,18 @@ public class Main {
                     System.out.println("Multiplication of " + a + " and " + b + " is : " + (a * b));
                     break;
                 case 4:
-                    System.out.println("Division of " + a + " and " + b + " is : " + (a/b));
+                    if(b==0){
+                        System.out.println("Division by zero is not possible");
+                    }else {
+                        System.out.println("Division of " + a + " and " + b + " is : " + (a / b));
+                    }
                     break;
                 case 5:
-                    System.out.println("Modulo of " + a + " and " + b + " is : " + (a % b));
+                    if(b==0){
+                        System.out.println("Modulo by zero is not possible");
+                    }else {
+                        System.out.println("Modulo of " + a + " and " + b + " is : " + (a % b));
+                    }
                     break;
                 default:
                     System.out.println("Invalid Operation");
